0f1cf9c53d6a8b1f5a57ead1a73625d5073d1f8a
Randy Gonzalez <jjwoo95@gmail.com>
2017-05-14 14:54:39 -0700

    HELLO wRLd

--- Src/UiMgr.cpp
+++ Src/UiMgr.cpp
@@ -238,8 +238,8 @@ void UiMgr::buttonHit(OgreBites::Button *b){
     	{
     		gameStart = true;
     		tensSecondsRemaining = 0;
-    		onesSecondsRemaining = 10;
-    		minutesRemaining = 0;
+    		onesSecondsRemaining = 0;
+    		minutesRemaining = 2;
     		lives = 3;
     		testLabel = mTrayMgr->createLabel(OgreBites::TL_BOTTOMRIGHT, "lives", livesText, 200);
     		fireballLabel = mTrayMgr->createLabel(OgreBites::TL_TOP, "fireball", fireballText, 200);
